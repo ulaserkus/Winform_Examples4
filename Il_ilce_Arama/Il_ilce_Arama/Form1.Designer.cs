7b0d305e658b4ee906331e2a74c7fb496ab03c63
Ulaş Hasan <67653664+ulaserkus@users.noreply.github.com>
2021-01-25 16:49:44 +0300

    Add files via upload

---
+++ Il_ilce_Arama/Il_ilce_Arama/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Il_ilce_Arama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_gettbl = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_connect.Location = new System.Drawing.Point(12, 12);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(126, 55);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "SQL Bağlan";
+            this.btn_connect.UseVisualStyleBackColor = false;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_gettbl
+            // 
+            this.btn_gettbl.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_gettbl.Location = new System.Drawing.Point(144, 12);
+            this.btn_gettbl.Name = "btn_gettbl";
+            this.btn_gettbl.Size = new System.Drawing.Size(126, 55);
+            this.btn_gettbl.TabIndex = 1;
+            this.btn_gettbl.Text = "Tablo Getir";
+            this.btn_gettbl.UseVisualStyleBackColor = false;
+            this.btn_gettbl.Click += new System.EventHandler(this.btn_gettbl_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_close.Location = new System.Drawing.Point(276, 12);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(126, 55);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.Text = "Bağlantı Kapat";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(500, 211);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(439, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Bağlantı Kapat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 296);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_gettbl);
+            this.Controls.Add(this.btn_connect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_gettbl;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Il_ilce_Arama/Il_ilce_Arama/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Il_ilce_Arama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection();
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            string adress = @"Data Source=USER\LOCAL;Initial Catalog=turkiye;Integrated Security=True";
+            if (connection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Bağlantı zaten açık");
+            }
+            else if(connection.State == ConnectionState.Closed)
+            {
+                connection.ConnectionString = adress;
+                connection.Open();
+            }
+            else
+            {
+                MessageBox.Show(connection.State.ToString());
+            }
+        }
+
+        private void btn_gettbl_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter data = new SqlDataAdapter("select * from tbl_il",connection);
+            DataTable dt = new DataTable();
+            data.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Search = new Search();
+            Search.Show();
+        }
+    }
+}
