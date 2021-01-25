fac7c03a56f227663d8d49567a68da1e9bb89be9
Ulaş Hasan <67653664+ulaserkus@users.noreply.github.com>
2021-01-25 16:52:00 +0300

    Add files via upload

---
+++ Il_ilce_Arama/Il_ilce_Arama/Search.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Il_ilce_Arama
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İlçe";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(170, 68);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(170, 118);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 230);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Search";
+            this.Text = "Search";
+            this.Load += new System.EventHandler(this.Search_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ Il_ilce_Arama/Il_ilce_Arama/Search.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Il_ilce_Arama
+{
+    public partial class Search : Form
+    {
+        static string cs = @"Data Source=USER\LOCAL;Initial Catalog=turkiye;Integrated Security=True";
+        public Search()
+        {
+            InitializeComponent();
+        }
+     
+        private void Search_Load(object sender, EventArgs e)
+        {
+            string command = "select * from tbl_il";
+            comboBox1.DisplayMember = "il_ad";
+            comboBox1.ValueMember = "il_id";
+            comboBox1.DropDownHeight = 150;
+            comboBox1.DataSource = Command(command, cs);
+
+        }
+
+        public static DataTable Command(string command, string cs)
+        {
+            SqlDataAdapter data = new SqlDataAdapter(command, cs);
+            DataTable dataTable = new DataTable();
+            data.Fill(dataTable);
+
+            return dataTable;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            short SelectedCountryId = (short)comboBox1.SelectedValue;
+            string cmd = "select ilce_ad from tbl_ilce where il_id="+SelectedCountryId.ToString();
+            comboBox2.DisplayMember = "ilce_ad";
+            comboBox2.DropDownHeight = 150;
+            comboBox2.DataSource = Command(cmd, cs);
+        }
+    }
+}
